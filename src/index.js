444885282fc70567bdaebf00598dccb51ae5fddd
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-02-15 18:56:12 +0100

    add regex valid github repo url checker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const myfirstelement = <h1>gitStory!</h1>
+function isGithubUrl(str) {
+  
+  let pattern1 = new RegExp('^(https://)?'+ // protocol
+    '(www[.])?'+ // 3w ?
+    '(github.com/)'+ // github
+    '([a-zA-Z0-9_-]+)/'+ // repo owner
+    '([a-zA-Z0-9_-]+)/*$','i'); // repo name
 
-// ReactDOM.render(myfirstelement, document.getElementById('root'));
+
+  let pattern2 = new RegExp('^(www[.])?'+ // 3w ?
+    '(github.com/)'+ // github
+    '([a-zA-Z0-9_-]+)/'+ // repo owner
+    '([a-zA-Z0-9_-]+)/*$','i'); // repo name
+
+  
+  let pattern3 = new RegExp('^(github.com/)'+ // github
+    '([a-zA-Z0-9_-]+)/'+ // repo owner
+    '([a-zA-Z0-9_-]+)/*$','i'); // repo name
+
+  return pattern1.test(str) || pattern2.test(str) || pattern3.test(str);
+}
+
+// set Visualize Button 
+let visualizeButton = document.getElementById('btn-submit-url');
+visualizeButton.onclick =  () => {
+    let repoUrl = document.getElementById('inp-repo-url').value; // get repo url
+    let isGithub = isGithubUrl(repoUrl);
+
+    if(!isGithub)
+    {
+        // show error message and clear input!
+    }
+    else
+    {
+        // send GET request to gh-api and check existence
+    }
+};
